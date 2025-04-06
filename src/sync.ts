bc15792c2bf53090f9b09328ca4b586fe836aac7
Davide Garberi <dade.garberi@gmail.com>
2025-04-06 17:15:24 +0200

    sync: Add workspaceDir

--- src/sync.ts
+++ src/sync.ts
@@ -2,10 +2,12 @@ import { Plugin } from "siyuan";
 
 export class SyncManager {
     private plugin: Plugin;
+    private localWorkspaceDir: string;
     private urlToKeyPairs: [string, string][];
 
-    constructor(plugin: Plugin, urlToKeyPairs: [string, string][] = []) {
+    constructor(plugin: Plugin, workspaceDir: string, urlToKeyPairs: [string, string][] = []) {
         this.plugin = plugin;
+        this.localWorkspaceDir = workspaceDir;
         this.urlToKeyPairs = urlToKeyPairs;
     }
 }
