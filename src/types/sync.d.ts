6d432503ee9b9f5d14d1ced325ffa33efb6106f1
Davide Garberi <dade.garberi@gmail.com>
2025-06-26 11:32:51 +0200

    types: sync: Add interfaces for document operations

--- src/types/sync.d.ts
+++ src/types/sync.d.ts
@@ -20,3 +20,32 @@ interface TransactionPayload {
     transactions: Transaction[];
     reqId: number;
 }
+
+interface CreateDocRequest {
+    notebook: string;
+    path: string;
+    title: string;
+    md: string;
+    listDocTree: boolean;
+}
+
+interface RenameDocRequest {
+    notebook: string;
+    path: string;
+    title: string;
+}
+
+interface RemoveDocRequest {
+    notebook: string;
+    path: string;
+}
+
+interface MoveDocsRequest {
+    fromPaths: string[];
+    toNotebook: string;
+    toPath: string;
+}
+
+interface RemoveDocsRequest {
+    paths: string[];
+}
